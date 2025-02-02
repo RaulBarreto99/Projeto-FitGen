68a1a9f15f56dd32adaeb3b2da03dfb98780cbc7
Raul Barreto <raul_barreto99@live.com>
2025-02-02 05:49:57 -0300

    add register popup and login popup

--- js/login.js
+++ js/login.js
@@ -1,3 +1,4 @@
+//Pop-up login
 $('#btnLogin').click(function () {
 
     $('.popup-overlay').css('display', 'block');
@@ -9,4 +10,18 @@ $('.popupClose').click(function () {
 
     $('.popup-overlay').css('display', 'none');
     $('.popupLogin').css('display', 'none');
+});
+
+//pop-up register
+$('#btnRegister').click(function () {
+
+    $('.popup-overlay-register').css('display', 'block');
+    $('.popupLogin-register').css('display', 'block');
+    $('.popupClose-register').css('display', 'block');
+});
+
+$('.popupClose-register').click(function () {
+
+    $('.popup-overlay-register').css('display', 'none');
+    $('.popupLogin-register').css('display', 'none');
 });
