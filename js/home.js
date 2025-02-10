3a8927bf6783f62cab3ee3cdf306048685c66d01
Raul Barreto <raul_barreto99@live.com>
2025-02-10 01:48:00 -0300

    trocando para modo branco

--- js/home.js
+++ js/home.js
@@ -1,7 +1,12 @@
+$('header').click(function () {
+    window.location.href = 'login.html';
+});
+
 $('.btnTrainingGenerate').click(function () {
     window.location.href = 'login.html';
 });
 
-$('header').click(function () {
-    window.location.href = 'login.html';
+$('.btnIMC').click(function () {
+    window.location.href = 'imc.html';
 });
+
